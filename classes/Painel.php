07ed92604f68de7513dd4dd50933076d0bd070f0
Arnobio Fernandes <arnobiopaulinofernandes@gmail.com>
2020-01-30 01:25:46 -0200

    alteracao sistema ordenacao

--- classes/Painel.php
+++ classes/Painel.php
@@ -2,13 +2,25 @@
 
 	class Painel{
 
-		public static function GetAll(){
+			public static function getAll(){
 			$sql = MySql::conectar()->prepare("SELECT * FROM ordenar");
 			$sql->execute();
-				if($sql->rowCount() > 0){
+			if($sql->rowCount() > 0){
+				return $sql->fetchAll();
+			}
+				
+		} 
+		
+
+		public static function orde($ordem){
+			$sql = MySql::conectar()->prepare("SELECT * FROM ordenar ORDER BY $ordem");
+			$sql->execute();
+			if($sql->rowCount() > 0){
 					return $sql->fetchAll();
 				}
 		}
+
+	
 
 		
 	}
--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-	include('confi.php');
-	$info = Painel::GetAll();
-
-
+include('confi.php');
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,19 +10,37 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 </head>
 <body>
+<?php
+  
+  
 
+  if(isset($_GET['ordem']) && !empty($_GET['ordem'])){
+    $ordem = addslashes($_GET['ordem']);
+    
+    $sql = MySql::conectar()->prepare("SELECT * FROM ordenar ORDER BY ".$ordem);
+        
+
+  }else{
+    $ordem = '';
+    $sql = MySql::conectar()->prepare("SELECT * FROM ordenar");
+    
+    
+    
+  }
+
+
+?>
 
 
 <div class="container">
-
-	<select name="ordem">
-	<option></option>
-	<option></option>
-	<option value="nome">Pelo Nome</option>
-	<option value="idade">Pela Idade</option>
+  <form method="GET">
+  	<select name="ordem" onchange="this.form.submit()">
+  	<option></option>
+  	<option value="nome"<?php echo($ordem == "nome")?'selected="selected"':'';?>>Pelo Nome</option>
+  	<option value="idade"<?php echo($ordem == "idade")?'selected="selected"':'';?>>Pela Idade</option>
 	
-</select>
- 
+  </select>
+ </form>
   <table class="table">
     <thead class="thead-dark">
       <tr>
@@ -35,17 +49,22 @@
       </tr>
     </thead>
     <?php 
-    	foreach ($info as $key => $value) {
-    		?>
-    		 <tbody>
-      		<tr>
-        		<td><?php echo $value['nome'];?></td>
-       			<td><?php echo $value['idade'];?></td>
-      		</tr>
+      $sql->execute();
+      if($sql->rowCount() > 0){
+          foreach ($sql as $key => $value) {
+        ?>
+         <tbody>
+          <tr>
+            <td><?php echo $value['nome'];?></td>
+            <td><?php echo $value['idade'];?></td>
+          </tr>
       
     </tbody>
-    		<?php
-    	}
+        <?php
+      }
+      }
+    
+
     ?>
    
   </table>
